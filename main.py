4b398728aa4a96f5ada9e723cf4adcb299d3506e
lim <sherwinlim8899@gmail.com>
2022-05-24 18:23:23 +0800

    5th commit

--- main.py
+++ main.py
@@ -3,12 +3,16 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-name = input("Name: ")
-age = int(input("Age: "))
-color = input("Favorite color: ")
-movie = input("Favorite movie: ")
-phonenum = int(input("Mobile number: "))
-motto = input("Motto in life: ")
+x = 10 > 7
+print(x)
+
+print(str(73911))
+
+print(tuple("Thank God it's Friday!"))
+
+print(float(4302))
+
+print(int(3299.35640))
 
 
 
