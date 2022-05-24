b8761192b5fe01f180c6787ed0238d3e6bd9b0a9
lim <sherwinlim8899@gmail.com>
2022-05-24 17:04:03 +0800

    2nd commit

--- main.py
+++ main.py
@@ -3,15 +3,16 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-char_job = "pirate"
-char_ride = "ship"
-char_souvenir = "gold"
-char_pet = "parrot"
+print("Roses are red, violets are blue")
 
-print("There was once a " + char_job + " who loved adventure.")
-print("The "+ char_job + " would take her " + char_ride + " to unknown places.")
-print("She brings home a lot of " + char_souvenir + ".")
-print("Then she goes home to her pet " + char_pet + ", Chuckles.")
+if 10 > 7:
+    print("Ten is greater than seven!")
+if 16 < 42:
+    print("Sixteen is less than forty-two!")
+
+print("A long time ago in a galaxy far, far away...")
+
+
 
 
 
