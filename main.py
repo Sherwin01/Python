5f88d9d651a52e49d5c284d75ad8b0dd851c3113
lim <sherwinlim8899@gmail.com>
2022-05-24 17:24:14 +0800

    3rd commit

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-print("Roses are red, violets are blue")
+def function1(name):
+    print("Have a nice day " + name + "!")
 
-if 10 > 7:
-    print("Ten is greater than seven!")
-if 16 < 42:
-    print("Sixteen is less than forty-two!")
 
-print("A long time ago in a galaxy far, far away...")
+function1("Lim")
+
+
+
 
 
 
