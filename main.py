49f455c03e84e2e6c6b16f39476d2cc2e40c6a33
lim <sherwinlim8899@gmail.com>
2022-05-24 09:38:43 +0800

    1st commit

--- main.py
+++ main.py
@@ -3,5 +3,16 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-print('I love cakes')
+char_job = "pirate"
+char_ride = "ship"
+char_souvenir = "gold"
+char_pet = "parrot"
+
+print("There was once a " + char_job + " who loved adventure.")
+print("The "+ char_job + " would take her " + char_ride + " to unknown places.")
+print("She brings home a lot of " + char_souvenir + ".")
+print("Then she goes home to her pet " + char_pet + ", Chuckles.")
+
+
+
 
