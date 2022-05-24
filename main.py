9a44a886d5d3cd1a1f8c170f6bf3d7f9e6fc46e5
lim <sherwinlim8899@gmail.com>
2022-05-24 18:58:28 +0800

    6th commit

--- main.py
+++ main.py
@@ -3,16 +3,27 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-x = 10 > 7
-print(x)
+class Customers:
+    greeting = "Welcome to the Coffee Place!"
 
-print(str(73911))
 
-print(tuple("Thank God it's Friday!"))
+c_1 = Customers()
 
-print(float(4302))
+c_1.name = "Samirah"
+c_1.beverage = "Iced caffe latte"
+c_1.food = "Cinnamon roll"
+c_1.total = "225"
 
-print(int(3299.35640))
+c_2 = Customers()
+
+c_2.name = "Jerry"
+c_2.beverage = "Caramel macchiato"
+c_2.food = "Glazed doughnut"
+c_2.total = "230"
+
+print(Customers.greeting)
+print(c_1.beverage)
+print(c_2.food)
 
 
 
