3ccf52bf9d77d3affa26dc395784d8986078c340
lim <sherwinlim8899@gmail.com>
2022-05-24 20:00:32 +0800

    10th commit

--- main.py
+++ main.py
@@ -3,16 +3,18 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-x = 332
-y = 2031
+furniture = ["table", "chair", "cabinet", "desk", "couch"]
+for x in furniture:
+    if x == "cabinet":
+        continue
+    print(x)
 
-if x >= y:
-    print("x is greater than or equal to y")
-elif x == y:
-    print("x is equal to y")
+i = 1
+while i < 15:
+    print(i)
+    i += 1
 else:
-    print("x is less than y")
-
+    print("i is no longer less than 15")
 
 
 
